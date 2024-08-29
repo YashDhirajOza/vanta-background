a007941a2749a2c6958723e506e9bb7796faaaaa
Yash Dhiraj Oza <144452060+YashDhirajOza@users.noreply.github.com>
2024-08-29 15:21:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+VANTA.GLOBE({
+    el: "#vanta-bg",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: true,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x3f3fff,
+    color2: 0xff00ff,
+    size: 1.50,
+    backgroundColor: 0x0
+});
